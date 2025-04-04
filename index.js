3cb42d116cbcf5b27606390856855b44a84087f5
root <root@play451.local>
2025-04-04 16:20:25 +0000

    why did i need to suffer

---
+++ index.js
@@ -0,0 +1,92 @@
+import express from "express";
+import { createServer } from "node:http";
+import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
+import { libcurlPath } from "@mercuryworkshop/libcurl-transport";
+import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
+import { join } from "node:path";
+import { hostname } from "node:os";
+import { server as wisp } from "@mercuryworkshop/wisp-js/server";
+
+const __dirname = process.cwd();
+const app = express();
+
+const publicPath = join(__dirname, "public");
+app.use(express.static(publicPath));
+
+const server = createServer();
+
+app.use("/libcurl/", express.static(libcurlPath));
+app.use("/baremux/", express.static(baremuxPath, {
+    extensions: ['js'],
+    index: 'index.js'
+}));
+
+app.use("/epoxy/", express.static(epoxyPath, {
+    extensions: ['js'],
+    index: 'index.js',
+    type: 'application/javascript'
+}));
+
+app.get("/baremux/index.js", (req, res) => {
+    res.sendFile(join(baremuxPath, "index.js"), { headers: { 'Content-Type': 'application/javascript' } });
+});
+
+app.get("/epoxy/index.js", (req, res) => {
+    res.sendFile(join(epoxyPath, "index.js"), { headers: { 'Content-Type': 'application/javascript' } });
+});
+
+app.get("/libcurl/index.js", (req, res) => {
+    res.sendFile(join(libcurlPath, "index.js"), { headers: { 'Content-Type': 'application/javascript' } });
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(join(publicPath, "index.html"));
+});
+
+app.get("*", (req, res) => {
+    res.status(404);
+    res.sendFile(join(publicPath, "index.html"));
+});
+
+server.on("request", (req, res) => {
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+    res.setHeader("Cross-Origin-Embedder-Policy", "anonymous");
+    app(req, res);
+});
+
+server.on("upgrade", (req, socket, head) => {
+    wisp.routeRequest(req, socket, head);
+});
+
+let port = parseInt(process.env.PORT || "3001");
+
+if (isNaN(port)) port = 3001;
+
+server.on("listening", () => {
+    const address = server.address();
+
+    // by default we are listening on 0.0.0.0 (every interface)
+    // we just need to list a few
+    console.log("Listening on:");
+    console.log(`\thttp://localhost:${address.port}`);
+    console.log(`\thttp://${hostname()}:${address.port}`);
+    console.log(
+        `\thttp://${address.family === "IPv6" ? `[${address.address}]` : address.address
+        }:${address.port}`
+    );
+
+});
+
+// https://expressjs.com/en/advanced/healthcheck-graceful-shutdown.html
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+function shutdown() {
+    console.log("SIGTERM signal received: closing HTTP server");
+    server.close();
+    process.exit(0);
+}
+
+server.listen({
+    port,
+});
