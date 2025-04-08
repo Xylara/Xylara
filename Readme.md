1b203c8d08052ab6ac694d4558a0ba5c04121328
root <root@play451.local>
2025-04-08 02:29:27 +0000

    it works now

--- Readme.md
+++ Readme.md
@@ -4,6 +4,13 @@ A proxy site with some features
 
 > Im still working on this so dont make a issue saying how to switch to scramjet. Scramjet itself is implimented you just need to switch the proxy via console which I assume most of you dont have.
 
+# Features
+
+- Ultraviolet
+- Scramjet
+- Transport switching
+- More coming soon
+
 # Self-Deployment
 
 ## With node
@@ -27,3 +34,7 @@ docker run -p 80:3001 xylara
 ```
 > You can change 80 to whatever port you like
 
+# Credits
+
+Titanium Network - Ultraviolet
+Mercury Workshop - Srcamjet, Baremux, Wisp, Epoxy, Libcurl
--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,10 @@
 importScripts('/uv/uv.bundle.js');
 importScripts('/uv/uv.config.js');
 importScripts('/uv/uv.sw.js');
-importScripts('/meteor/meteor.codecs.js')
-importScripts('/meteor/meteor.config.js')
-importScripts('/meteor/meteor.bundle.js')
-importScripts('/meteor/meteor.worker.js')
 importScripts("/scram/scramjet.wasm.js", "/scram/scramjet.shared.js", "/scram/scramjet.worker.js");
 
 const uv = new UVServiceWorker();
 const scramjet = new ScramjetServiceWorker();
-const meteor = new MeteorServiceWorker()
 
 let playgroundData;
 
@@ -28,10 +23,6 @@ async function handleRequest(event) {
     if (scramjet.route(event)) {
         return await scramjet.fetch(event);
     }
-
-    if (meteor.shouldRoute(event)) {
-        return meteor.handleFetch(event)
-      }
     return await fetch(event.request);
 }
 
