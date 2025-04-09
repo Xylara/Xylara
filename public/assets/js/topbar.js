185002dd61a91fd3b2b563323e370378fef7a938
root <root@play451.local>
2025-04-09 14:46:37 +0000

    settings page started

--- public/assets/js/topbar.js
+++ public/assets/js/topbar.js
@@ -2,12 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const topbarItem = document.querySelector('.topbar-item');
     const currentPath = window.location.pathname;
 
-    if (currentPath === '/') {
+    const activePaths = ['/', '/settings'];
+    if (activePaths.includes(currentPath)) {
         topbarItem.classList.add('active');
     }
-
-    window.addEventListener('click', () => {
-        const currentPath = window.location.pathname;
-        topbarItem.classList.toggle('active', currentPath === '/');
-    });
 });
